7b0c1b6eaa73a8c1bc43b394c8e9dd5aa2315fbb
Legagneur <Louis6029>
2021-01-08 14:06:05 +0100

    update

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = 'logo.png';
+logo.src = 'studio-ghibli-logo.svg';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -9,12 +9,12 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
+  const data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     let i = 0;
     for (i ;i < data.length; ++i) {
